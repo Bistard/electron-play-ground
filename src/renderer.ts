a7cf5b60cb9a88a74314f5b3b001596190be9300
SIHAN LI <1015813038@qq.com>
2022-01-16 12:18:04 -0500

    [Enviro] install mocha

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,4 +1,15 @@
+import { Scrollable } from "src/base/browser/basic/scrollable/scrollable";
+import { requestAnimationFrame } from "src/base/common/animation";
+
+// [basic]
+
 const container = document.createElement('div');
 container.className = 'container';
 document.body.appendChild(container);
 
+// [end]
+
+const scrollable = new Scrollable(125);
+
+let count = 0;
+requestAnimationFrame(() => {console.log(window || self || 'undefine')});
