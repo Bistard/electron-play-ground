101e46a438b5eb41001330a7dde6837142fb31cc
SIHAN LI <1015813038@qq.com>
2022-01-18 02:16:45 -0500

    [New] `types.ts`

---
+++ src/base/common/types.ts
@@ -0,0 +1,33 @@
+
+export function isObject(obj: any): obj is any {
+    return typeof obj === "object"
+        && obj !== null
+        && !Array.isArray(obj)
+        && !(obj instanceof RegExp)
+        && !(obj instanceof Date);
+}
+
+export function isEmptyObject(obj: any): boolean {
+    if (!isObject(obj)) {
+        return false;
+    }
+
+    for (const key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+export function isArray(array: any): array is any[] {
+	return Array.isArray(array);
+}
+
+export function ifOrDefault<T>(value: T, defaultValue: T): T {
+    if (typeof value === 'undefined') {
+        return defaultValue;
+    }
+    return value;
+}
