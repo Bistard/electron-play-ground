709487166cf01888c6927b33a904942ed19c8569
SIHAN LI <1015813038@qq.com>
2022-02-04 11:52:10 -0500

    [update] scrollable auto fire event

--- src/render.ts
+++ src/render.ts
@@ -1,7 +1,71 @@
-import { ListView } from "src/base/browser/secondary/listView/listView";
-import { IScrollableWidgetCreationOpts, IScrollableWidgetExtensionOpts } from "src/base/browser/secondary/scrollableWidget/scrollableWidgetOptions";
-import { DOMSize } from "src/base/common/dom";
-import { IRange } from "src/base/common/range";
+import { IListViewRenderer, ListViewRendererType } from "src/base/browser/secondary/listView/listRenderer";
+import { ListView, ViewItemType } from "src/base/browser/secondary/listView/listView";
+import { IScrollableWidgetExtensionOpts } from "src/base/browser/secondary/scrollableWidget/scrollableWidgetOptions";
+
+// [test]
+
+const buildColors = function(start: number[], end: number[], count: number): number[][] {
+    //Distance between each color
+    var steps = [
+      (end[0]! - start[0]!) / count,  
+      (end[1]! - start[1]!) / count,  
+      (end[2]! - start[2]!) / count  
+    ]!;
+    
+    //Build array of colors
+    var colors = [start];
+    for(var ii = 0; ii < count - 1; ++ii) {
+      colors.push([
+        Math.floor(colors[ii]![0]! + steps[0]!),
+        Math.floor(colors[ii]![1]! + steps[1]!),
+        Math.floor(colors[ii]![2]! + steps[2]!)
+      ]);
+    }
+    colors.push(end); 
+   
+    return colors;
+};
+
+const nodeCount = 10;
+const colors = buildColors([255, 255, 0], [0, 255, 255], nodeCount);
+let testNodeIndex = 0;
+
+export class TestNode {
+
+    public index: number;
+    public size: number;
+    public color: string;
+    
+    constructor(index: number, size?: number) {
+        this.index = index;
+        this.size = size ?? 100;
+        this.color = 'rgb(' + colors[testNodeIndex++]!.join(',') + ')';
+    }
+
+}
+
+export class TestRenderer implements IListViewRenderer {
+
+    readonly type: ViewItemType = ListViewRendererType.TEST;
+
+    constructor() {
+
+    }
+
+    public render(element: HTMLElement, node: TestNode): void {
+        element.style.height = node.size + 'px';
+        element.style.background = node.color;
+
+        const label = document.createElement('div');
+        label.className = 'test-node-label';
+        label.innerText = node.index.toString();
+        element.appendChild(label);
+    }
+
+    public dispose(element: HTMLElement): void {
+        
+    }
+}   
 
 // [basic]
 
@@ -9,64 +73,24 @@ const scrollableElement = document.createElement('div');
 scrollableElement.className = 'container';
 document.body.appendChild(scrollableElement);
 
-const scrollbarElement = document.createElement('div');
-scrollableElement.appendChild(scrollbarElement);
-
 // [end]
-
-const creationOpts: IScrollableWidgetCreationOpts = {
-    viewportSize: DOMSize.getContentHeight(scrollableElement),
-    scrollSize: DOMSize.getContentHeight(scrollableElement) * 10,
-    scrollPosition: 0,
-};
 
 const extensionOpts: IScrollableWidgetExtensionOpts = {
     mouseWheelScrollSensibility: 0.5
 };
 
-const listView = new ListView(scrollableElement, {
-    transformOptimization: true
-});
+const listView = new ListView<TestNode>(
+    scrollableElement, 
+    [new TestRenderer()], 
+    {
+        transformOptimization: true,
+        mouseWheelScrollSensitivity: 0.5
+    }
+);
 
-/**
- * 
- */
+const items: TestNode[] = [];
+for (let i = 0; i < nodeCount; i++) {
+    items.push(new TestNode(i));
+}
 
-const r1: IRange = {start: 0, end: 100};
-const r2: IRange = {start: 50, end: 150};
-const r3: IRange = {start: 0, end: 50};
-const r4: IRange = {start: 50, end: 200};
-
-// const rg1: IRangedGroup = {
-//     range: r1,
-//     size: 10
-// };
-
-// const rg2: IRangedGroup = {
-//     range: r2,
-//     size: 5
-// };
-
-// const rg3: IRangedGroup = {
-//     range: r3,
-//     size: 20
-// };
-
-/**
- * @function Range.intersect()
- * @description Simply calculate the union of two ranges.
- */
-// console.log(Range.intersect(r1, r2));
-// console.log(Range.intersect(r1, r3));
-// console.log(Range.intersect(r1, r4));
-// console.log(Range.intersect(r2, r3));
-// console.log(Range.intersect(r2, r4));
-// console.log(Range.intersect(r3, r4));
-
-/**
- * @function groupIntersect()
- * @description Compares the given range to each of the RangedGroup, if they 
- * intersect, push the result intersection with the rangedGroup size into an 
- * array.
- */
-// console.log(groupIntersect(r1, [rg1, rg2]));
+listView.splice(0, 0, items);
